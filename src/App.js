58335fd5b9d8125bd9fbcda43003c4073d245d6d
Yunip Shrestha <yunipshrestha@gmail.com>
2022-03-07 00:09:41 +0545

    navbar completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <Navbar />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
