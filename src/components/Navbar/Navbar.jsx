5f6f1f54240d71be0c1c6b4f4d813c4ecbb4065d
Yunip Shrestha <yunipshrestha@gmail.com>
2022-03-06 23:22:03 +0545

    Components folder added

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <div className="nav-brand">MedBeema</div>
+
+        <ul>
+          <li>Get Cover</li>
+          <li>Tokens</li>
+          <li>Claims</li>
+        </ul>
+
+        <button>
+          
+        </button>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
