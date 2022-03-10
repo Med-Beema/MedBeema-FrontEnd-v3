906fe229f6b34f791445ff9a050a4fa5c13b9731
garima <garimasubedi321@gmail.com>
2022-03-11 02:32:03 +0545

    my details

--- src/components/Stake/Stake.jsx
+++ src/components/Stake/Stake.jsx
@@ -5,13 +5,13 @@ export default function Stake() {
     <div>
       <div className="flex justify-between">
         <div className="px-40 mt-20 grid place-items-left bg-[#f9f9fe]">
-          <div class="w-[500px] h-[350px] rounded shadow-md shadow-[4px 4px 4px rgba(0, 0, 0, 0.25)] bg-[#ffffff]">
-            <div class="m-10 space-y-4">
+          <div className="w-[500px] h-[350px] rounded shadow-md shadow-[4px 4px 4px rgba(0, 0, 0, 0.25)] bg-[#ffffff]">
+            <div className="m-10 space-y-4">
               <label class="text-black text-lg font-regular" for="username">
                 Number of Tokens
               </label>
               <input
-                class="h-14 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight border-purple focus:outline-purple focus:shadow-outline"
+                className="h-14 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight border-purple focus:outline-purple focus:shadow-outline"
                 type="numeric"
               ></input>
             </div>
@@ -24,13 +24,13 @@ export default function Stake() {
           </div>
         </div>
         <div className="px-40 mt-20 grid place-items-left bg-[#f9f9fe]">
-          <div class="w-[500px] h-[350px] rounded shadow-md shadow-[4px 4px 4px rgba(0, 0, 0, 0.25)] bg-[#ffffff]">
+          <div className="w-[500px] h-[350px] rounded shadow-md shadow-[4px 4px 4px rgba(0, 0, 0, 0.25)] bg-[#ffffff]">
             <div class="m-10 space-y-4">
-              <label class="text-black text-lg font-regular" for="username">
+              <label className="text-black text-lg font-regular" for="username">
                 Number of Tokens
               </label>
               <input
-                class="h-14 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight border-purple focus:outline-purple focus:shadow-outline"
+                className="h-14 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight border-purple focus:outline-purple focus:shadow-outline"
                 type="numeric"
               ></input>
             </div>
--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import AllCovers from "../components/AllCovers/AllCovers";
 import ClaimResult from "../components/Claims/ClaimResult";
+import Claims from "../components/Claims/Claims";
 import ClaimsDetails from "../components/Claims/ClaimsDetails";
 import MyDetails from "../components/MyDetails/MyDetails";
+import MyDetailsFront from "../components/MyDetails/MyDetailsFront";
 import Stake from "../components/Stake/Stake";
 import ClaimsPage from "../views/ClaimsPage";
 import CoverPage from "../views/CoverPage";
@@ -13,9 +15,12 @@ function AppRoute() {
   return (
     <Routes>
       <Route exact path="/" element={<CoverPage />}>
-        <Route path="" element={<MyDetails />} />
+        <Route path="" element={<MyDetailsFront />}></Route>
         <Route path="AllCovers" element={<AllCovers />} />
       </Route>
+      <Route path="/MyCovers" element={<AllCovers />} />
+      <Route path="/Claims" element={<Claims />} />
+      <Route path="/KYC" element={<MyDetails />} />
       <Route path="/tokens" element={<TokenPage />} />
       <Route path="/claims" element={<ClaimsPage />} />
       <Route path="/claimdetails" element={<ClaimsDetails />} />
